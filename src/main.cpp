890e795fc64e758884ac73717c5f12b7d9d43e2c
SAM SAM <37674434-attaleysamuel@users.noreply.replit.com>
2024-12-20 11:52:01 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+ cout << "************ Evaluation 2 ************" << endl << endl;  cout << "1 - Entrée dans le parking"<< endl; 
+ cout << "2 - Nombre de places disponibles"<< endl;
+ cout << "3 - Sortie du parking"<< endl;
+ cout << "4 - quitter"<< endl;
+ return 0;
+}
